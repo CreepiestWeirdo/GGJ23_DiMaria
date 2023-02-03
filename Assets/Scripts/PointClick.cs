2b283d2cf4d4fe5fac0a04320966382c18b15537
Federico Poliseno <89668474+CreepiestWeirdo@users.noreply.github.com>
2023-02-03 10:07:28 -0300

    Mecánica para Point and Click

---
+++ Assets/Scripts/PointClick.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour
+{
+    // Referencia a la camara
+    private Camera cam;
+
+    void Start()
+    {
+        // Obtiene el componente
+        cam = Camera.main;
+    }
+
+    void Update()
+    {
+        // Si se clickea el mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            // Crea un ray desde la posición del mouse
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            // Usa Physics2D.Raycast para detectar objetos en escena
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            // Detectacta si el Raycast toca un objeto
+            if (hit.collider != null)
+            {
+                // Hace algo con el objeto alcanzado
+                hit.collider.gameObject.SendMessage("OnClick");
+            }
+        }
+    }
+}
