b2582427b44b4f2aed4c70e28f972042f84db1dd
Federico Poliseno <89668474+CreepiestWeirdo@users.noreply.github.com>
2023-02-04 18:42:12 -0300

    ajuste

--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -4,32 +4,13 @@ public class PlayerCharacter : MonoBehaviour
 {
     public float speed = 10.0f;
     private Vector3 targetPosition;
-    public float grabRadius = 2.0f; //a partir de que distancia el objeto es interactuable
-    private GameObject grabObject;
-
-    
+        
     void Update()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            Collider2D[] hitObjects = Physics2D.OverlapCircleAll(transform.position, grabRadius);
-            foreach (Collider2D hitObject in hitObjects)
-            {
-                //si el tag del objeto coincide, se agarra
-                if(hitObject.gameObject.CompareTag("Grabbable"))        
-                {
-                    grabObject = hitObject.gameObject;
-                    grabObject.transform.parent=transform;
-                    break;                
-                //sino se mueve hacia el lugar
-                } else 
-                {
-                    targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    targetPosition.z = transform.position.z;
-                    break;
-                }
-            }
-            
+            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPosition.z = transform.position.z;            
         }
 
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
@@ -39,6 +20,5 @@ public class PlayerCharacter : MonoBehaviour
     {
         Debug.Log("Player collided with " + other.gameObject.name),
     }
-
 
 }
